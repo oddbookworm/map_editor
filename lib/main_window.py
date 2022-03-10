e68e90411de64d1ffea33bbfd7a7f30d58107e81
Andrew Coffey <andrewryancoffey@hotmail.com>
2022-03-09 21:07:51 -0600

    worked on layout and loading actual tiles

--- lib/main_window.py
+++ lib/main_window.py
@@ -1,11 +1,13 @@
 import pygame as pg
 import pygame_gui as pg_g
 import pygame_menu as pg_m
-
+from os import path
 try:
     from tile import Tile
+    from tile_loader import SpriteSheetReader
 except ModuleNotFoundError:
     from .tile import Tile
+    from .tile_loader import SpriteSheetReader
 
 class MainWindow:
     def __init__(self, size):
@@ -67,3 +69,46 @@ class MainWindow:
         self.menu.add.selector('Difficulty : ', [('Hard', 1), ('Easy', 2)])
         self.menu.add.button('Play', lambda : print('test'))
         self.menu.add.button('Quit', pg_m.events.EXIT)
+
+    def load_tiles(self, tileset, og_size, tile_size):
+        gap = 5
+        
+        parent_dir = path.abspath(path.join(path.dirname(__file__), ".."))
+        reader = SpriteSheetReader(''.join([parent_dir, tileset]), og_size)
+        width, height = reader.get_ss_dims()
+
+        cols = 3
+        if width * height % 3:
+            rows = (width * height) % 3
+        else:
+            rows = ((width * height) // 3) + 1
+        
+        width, height = (tile_size * cols + gap * (cols + 1),
+                        tile_size * rows + gap * (rows + 1))
+        self.tile_surf = pg.Surface((width, height))
+        self.tile_surf_rect = self.tile_surf.get_rect()
+        self.tile_surf_rect.topleft = (0, 0)
+
+        size = (tile_size, tile_size)
+        self.tiles = []
+
+        i = 0
+        for row in range(height):
+            for col in range(width):
+                tile = reader.get_tile((col, row))
+                tile = reader.load_pygame(tile)
+                if tile is not None:
+                    col_drawn = i % 3
+                    row_drawn = (i - i % 3) // 3
+                    x = col_drawn * tile_size + (col_drawn + 1) * gap
+                    y = row_drawn * tile_size + (row_drawn + 1) * gap
+                    pos = (x, y)
+                    new_tile = Tile(size, pos, tile)
+                    self.tiles.append(new_tile)
+                    i += 1
+
+    def draw_tiles(self):
+        self.tile_surf.fill((0, 0, 0, 0))
+        for tile in self.tiles:
+            self.tile_surf.blit(tile.image, tile.rect)
+        self.screen.blit(self.tile_surf, self.tile_surf_rect)
